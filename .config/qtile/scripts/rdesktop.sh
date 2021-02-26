0394a531c1287da3fc110b47af8168ec8413d796
Alejandro Zabaleta <zabapuen@gmail.com>
2021-02-26 21:40:37 +0100

    Carga inicial

---
+++ .config/qtile/scripts/rdesktop.sh
@@ -0,0 +1,25 @@
+#!/bin/sh
+
+rdesktop \
+    -g 100% \
+    -P \
+    -r sound:local \
+    -d REDOCU \
+    -u alejandro.zabaleta \
+    -p P@ssw0rd \
+    -z \
+    -x b \
+    -M \
+    192.168.1.50:3389
+
+# rdesktop \
+#     -g 100% \               ## desktop geometry (WxH[@DPI][+X[+Y]])
+#     -P \                    ## use persistent bitmap caching
+#     -r sound:local \        ## enable specified device redirection
+#     -d REDOCU \             ## domain
+#     -u alejandro.zabaleta \ ## user name
+#     -p P@ssw0rd \           ## password
+#     -z \                    ## enable rdp compression
+#     -x b \                  ## RDP5 experience (m[odem 28.8], b[roadband], l[an] or hex nr.)
+#     -M \                    ## use local mouse cursor
+#     192.168.1.50:3389       ## server[:port]
