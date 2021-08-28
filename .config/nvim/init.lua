cdf97582ec78cd5ff9d196aa1cbc12a5e33e7950
Alejandro Zabaleta <zabapuen@gmail.com>
2021-08-28 13:43:47 +0200

    Fix

--- .config/nvim/init.lua
+++ .config/nvim/init.lua
@@ -7,3 +7,13 @@ for i = 1, #chad_modules, 1 do
     pcall(require, chad_modules[i])
 end
 
+-- local cmd = vim.cmd
+-- local g = vim.g
+-- 
+-- g.mapleader = " "
+-- g.auto_save = 0
+-- 
+-- -- colorscheme related stuff
+-- cmd "syntax on"
+-- cmd "10new +terminal | setlocal nobuflisted"
+-- cmd "NvimTreeToggle"
